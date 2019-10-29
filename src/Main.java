44fdf6b417c2777fc86226f366b652d505a6024e
petivip <petivip@gmail.com>
2019-10-29 10:48:21 -0400

    add extension exercise

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -5,9 +7,22 @@ public class Main {
     public static String userInput="";
     public static int count=0;
     public static boolean key=true;
+    public static int earning=0;
+    public static HashMap<String, Boolean> myMap = new HashMap<String, Boolean>();
+    public static String[] arr=new String[10];
+    public static int i=0;
+
     public static void main(String[] args) {
 
         Scanner kd = new Scanner(System.in);
+        myMap.put("foyer",true);
+        myMap.put("front room",true);
+        myMap.put("library",true);
+        myMap.put("kitchen",true);
+        myMap.put("dining room",true);
+        myMap.put("vault",true);
+        myMap.put("parlor",true);
+        myMap.put("secret room",true);
         foyer();
         while(!userInput.equalsIgnoreCase("word")) {
             switch (userInput) {
@@ -46,8 +61,15 @@ public class Main {
     public static String foyer(){
         Scanner kd = new Scanner(System.in);
         count+=1;
+        if(myMap.get("foyer")) {
+            earning += getRandomNumberInRange(0, 1000);
+            myMap.put("foyer",false);
+            arr[i]="dead scorpion";
+            i++;
+        }
         System.out.println("You are in room 1");
         System.out.println("dead scorpion");
+        System.out.println("Until now you have earned: " + earning);
         System.out.println("Do you want to go north or quit?");
         String answer= kd.next();
         if(answer.equalsIgnoreCase("north")){
@@ -61,8 +83,15 @@ public class Main {
     public static String frontRoom(){
         Scanner kd = new Scanner(System.in);
         count+=1;
+        if(myMap.get("front room")) {
+            earning += getRandomNumberInRange(0, 1000);
+            myMap.put("front room",false);
+            arr[i]="piano";
+            i++;
+        }
         System.out.println("You are in room 2");
         System.out.println("piano");
+        System.out.println("Until now you have earned: " + earning);
         System.out.println("Do you want to go south,west, east or quit?");
         String answer= kd.next();
         switch (answer) {
@@ -87,8 +116,15 @@ public class Main {
     public static String library(){
         Scanner kd = new Scanner(System.in);
         count+=1;
+        if(myMap.get("library")) {
+            earning += getRandomNumberInRange(0, 1000);
+            myMap.put("library",false);
+            arr[i]="spiders";
+            i++;
+        }
         System.out.println("You are in room 3");
         System.out.println("spiders");
+        System.out.println("Until now you have earned: " + earning);
         System.out.println("Do you want to go north, east or quit?");
         String answer= kd.next();
         switch (answer) {
@@ -110,8 +146,15 @@ public class Main {
     public static String kitchen(){
         Scanner kd = new Scanner(System.in);
         count+=1;
+        if(myMap.get("kitchen")) {
+            earning += getRandomNumberInRange(0, 1000);
+            myMap.put("kitchen",false);
+            arr[i]="bats";
+            i++;
+        }
         System.out.println("You are in room 4");
         System.out.println("bats");
+        System.out.println("Until now you have earned: " + earning);
         System.out.println("Do you want to go west, north or quit?");
         String answer= kd.next();
         switch (answer) {
@@ -133,8 +176,15 @@ public class Main {
     public static String diningRoom(){
         Scanner kd = new Scanner(System.in);
         count+=1;
+        if(myMap.get("dining room")) {
+            earning += getRandomNumberInRange(0, 1000);
+            myMap.put("dining room",false);
+            arr[i]="dust adn empty box";
+            i++;
+        }
         System.out.println("You are in room 5");
         System.out.println("dust and empty box");
+        System.out.println("Until now you have earned: " + earning);
         System.out.println("Do you want to go south or quit?");
         String answer= kd.next();
         switch (answer) {
@@ -154,10 +204,16 @@ public class Main {
     public static String vaultRoom() {
         Scanner kd = new Scanner(System.in);
         count += 1;
-
+        if(myMap.get("vault")) {
+            earning += getRandomNumberInRange(0, 1000);
+            myMap.put("vault",false);
+            arr[i]="3 walking skeletons";
+            i++;
+        }
 
         System.out.println("You are in room 6 - Vault");
         System.out.println("3 walking skeletons");
+        System.out.println("Until now you have earned: " + earning);
         if (key) {
             System.out.println("Do you want to go east?");
             String answer = kd.nextLine();
@@ -197,8 +253,15 @@ public class Main {
     {
         Scanner kd = new Scanner(System.in);
         count+=1;
+        if(myMap.get("parlor")) {
+            earning += getRandomNumberInRange(0, 1000);
+            myMap.put("parlor",false);
+            arr[i]="Treasure chest";
+            i++;
+        }
         System.out.println("You are in room 7 - Parlor");
         System.out.println("Treasure chest");
+        System.out.println("Until now you have earned: " + earning);
         System.out.println("Do you want to go west, south, or quit?");
         String answer = kd.nextLine();
         switch (answer) {
@@ -217,8 +280,15 @@ public class Main {
     public static String secretRoom() {
         Scanner kd = new Scanner(System.in);
         count+=1;
+        if(myMap.get("secret room")) {
+            earning += getRandomNumberInRange(0, 1000);
+            myMap.put("secret room",false);
+            arr[i]="pile of gold";
+            i++;
+        }
         System.out.println("You are in room 8");
-        System.out.println("pile if gold");
+        System.out.println("pile of gold");
+        System.out.println("Until now you have earned: " + earning);
         System.out.println("Do you want to go west or quit?");
         String answer = kd.nextLine();
         switch (answer) {
@@ -238,11 +308,22 @@ public class Main {
         int nextInt = random.nextInt(4);
         if (nextInt == 0) {
             System.out.println("A ghost is following you");
+
         }
-        System.out.println("You have visited :" + count);
+        System.out.println("The number of rooms you visited:" + count);
+        System.out.println("The amount of money you have earned :" + earning);
+        System.out.println("You have seen :" + Arrays.toString(arr));
         userInput="word";
         return userInput;
     }
+    public static int getRandomNumberInRange(int min, int max) {
 
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
 
 }
